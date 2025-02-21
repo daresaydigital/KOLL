e7ed573e4f66e15f14b8946bf2f4fcd251f2296b
William Danielsson <william.danielsson@knightec.se>
2025-02-21 10:35:37 +0100

    Pushing what I have

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Admin() {
+  return (
+    <>
+      <Head>
+        <title>Content Manager</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="noindex" />
+        {/* Netlify Identity Widget */}
+        <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+
+      <body>
+        {/* Decap CMS script (must be inside body) */}
+        <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
+      </body>
+    </>
+  );
+}
